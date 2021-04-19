40d05cc73c027aa09628ebdf22367582d0e0b187
GdoongMathew <dungdudu426@gmail.com>
2021-04-20 04:12:10 +0800

    add discriminator model and some piece of DEGAN

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-
+import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -90,13 +90,66 @@ def unet(input_dim, output_channels):
     _model = Model(inputs=inputs, outputs=x)
     return _model
 
+
+def discriminator(input_dim):
+    in_1 = Input(shape=input_dim)
+    in_2 = Input(shape=input_dim)
+    x1 = Conv2D(32, 3, activation='elu', padding='same')(in_1)
+    x1 = BatchNormalization()(x1)
+    x1 = Conv2D(32, 3, strides=2, activation='elu', padding='same')(x1)
+    x1 = BatchNormalization()(x1)
+
+    x2 = Conv2D(32, 3, activation='elu', padding='same')(in_2)
+    x2 = BatchNormalization()(x2)
+    x2 = Conv2D(32, 3, strides=2, activation='elu', padding='same')(x2)
+    x2 = BatchNormalization()(x2)
+
+    x = Concatenate()([x1, x2])
+    x = Conv2D(128, 5, activation='elu', padding='same')(x)
+    x = Conv2D(256, 5, strides=(2, 2), padding='same', activation='relu')(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(256, 5, activation='elu', padding='same')(x)
+    x = Conv2D(256, 5, strides=(2, 2), padding='same', activation='relu')(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(1, 1, activation='sigmoid', padding='same')(x)
+    _model = Model(inputs=[in_1, in_2], outputs=x)
+    return _model
+
+
+class DEGAN(Model):
+    def __init__(self, _generator, _discriminator):
+        super(DEGAN, self).__init__()
+        self._generator = _generator
+        self._discriminator = _discriminator
+
+    def compile(self, gen_optimizer, disc_optimizer, gen_loss_fn, disc_loss_fn):
+        super(DEGAN, self).compile()
+        self.gen_optimizer = gen_optimizer
+        self.disc_optimizer = disc_optimizer
+        self.gen_loss_fn = gen_loss_fn
+        self.disc_loss_fn = disc_loss_fn
+
+
+    def train_step(self, data):
+        noise_img, clean_img = data
+        with tf.GradientTape() as gen_taper, tf.GradientTape() as disc_tape:
+            generated_img = self._generator(noise_img, training=True)
+            real_output = self._discriminator()
+
+
+
 if __name__ == '__main__':
     input_dim = (256, 256, 3)
     out = 1
 
     m1 = cnn(input_dim, out)
     m2 = unet(input_dim, out)
+    d = discriminator(input_dim)
 
-    m1.summary()
-    print('===============================')
-    m2.summary()
+    # m1.summary()
+    # print('===============================')
+    # m2.summary()
+    # print('===============================')
+    d.summary()

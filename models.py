89b5a8a3e247c2e9c5c1619ae2a4a772c775afb0
GdoongMathew <dungdudu426@gmail.com>
2021-04-27 20:08:26 +0800

    add WGAN with gradient penalty support.

--- models.py
+++ models.py
@@ -11,6 +11,65 @@ from tensorflow.keras.layers import Concatenate
 from tensorflow.python.keras.engine import data_adapter
 
 
+def conv2d_bn(x,
+              filters,
+              kernels,
+              padding='same',
+              strides=1,
+              name=None,
+              activation='relu'):
+    if name is not None:
+        bn_name = name + '_bn'
+        conv_name = name + '_conv'
+        if isinstance(activation, str):
+            act_name = name + f'_{activation}'
+        elif hasattr(activation, 'name'):
+            act_name = name + f'_{activation.name}'
+        else:
+            act_name = name + f'_{repr(activation)}'
+    else:
+        bn_name = None
+        conv_name = None
+        act_name = None
+    x = Conv2D(
+        filters, kernels,
+        strides=strides,
+        padding=padding,
+        name=conv_name)(x)
+    x = BatchNormalization(name=bn_name)(x)
+    if activation is not None:
+        x = Activation(activation, name=act_name)(x)
+    return x
+
+
+def inception_block(x, mix=1, activation='relu'):
+    mix_filters = {
+        3: [4, 8, 12, 16, 24, 24],
+        2: [8, 12, 16, 24, 32, 32],
+        1: [12, 16, 24, 32, 48, 48],
+        0: [16, 24, 32, 48, 64, 64]
+    }
+    if mix not in mix_filters:
+        return x
+
+    name = f'inception_{mix}_'
+    filters = mix_filters[mix]
+    branch1 = conv2d_bn(x, filters[0], 1, activation=activation, name=name + 'b1_')
+
+    branch5 = conv2d_bn(x, filters[1], 1, activation=activation, name=name + 'b51_')
+    branch5 = conv2d_bn(branch5, filters[2], 5, activation=activation, name=name + 'b52_')
+
+    branch3 = conv2d_bn(x, filters[3], 1, activation=activation, name=name + 'b31_')
+    branch3 = conv2d_bn(branch3, filters[4], 3, activation=activation, name=name + 'b32_')
+    branch3 = conv2d_bn(branch3, filters[5], 3, activation=activation, name=name + 'b33_')
+
+    branch_pool = AveragePooling2D(strides=1, padding='same')(x)
+    branch_pool = conv2d_bn(branch_pool, filters[0], 1, activation=activation)
+
+    x = Concatenate()([branch1, branch5, branch3, branch_pool])
+    return x
+
+
 def cnn(input_dim, output_channels, name='CNN'):
     inputs = Input(shape=input_dim)
 
@@ -52,7 +111,7 @@ def cnn(input_dim, output_channels, name='CNN'):
     for ch in channels[::-1]:
         x = up_conv(x, ch)
 
-    x = Conv2D(output_channels, 3, activation='sigmoid', padding='same')(x)
+    x = Conv2D(output_channels, 3, activation='sigmoid', padding='same', dtype=tf.float64)(x)
     _model = Model(inputs=inputs, outputs=x, name=name)
     return _model
 
@@ -61,10 +120,11 @@ def unet(input_dim,
          output_channels,
          name='Unet',
          use_pooling=True,
-         final_activation='sigmoid'):
+         final_activation='sigmoid',
+         skip_layers=None):
     inputs = Input(shape=input_dim)
 
-    def conv_down(_x, channels):
+    def conv_down(_x, channels, layers_id):
         _x = Conv2D(channels, 3, padding='same')(_x)
         _x = Activation('elu')(_x)
         _x = Conv2D(channels, 3, padding='same')(_x)
@@ -75,6 +135,9 @@ def unet(input_dim,
         else:
             d = Conv2D(channels, 3, strides=(2, 2), padding='same')(_x)
             d = Activation('elu')(d)
+
+        if skip_layers == 'inception':
+            _x = inception_block(_x, layers_id, activation='elu')
 
         return d, _x
 
@@ -98,8 +161,8 @@ def unet(input_dim,
     channels = [64, 128, 256, 512]
 
     layers = []
-    for ch in channels:
-        x, cnv_ly = conv_down(x, ch)
+    for i, ch in enumerate(channels):
+        x, cnv_ly = conv_down(x, ch, i)
         layers.append(cnv_ly)
 
     x = Conv2D(1024, 3, padding='same')(x)
@@ -111,56 +174,51 @@ def unet(input_dim,
     for ch, ly in zip(channels[::-1], layers[::-1]):
         x = conv_up(x, ly, ch)
 
-    x = Conv2D(output_channels, 1, activation=final_activation, padding='same')(x)
+    x = Conv2D(output_channels, 1, activation=final_activation, padding='same', dtype=tf.float64)(x)
     _model = Model(inputs=inputs, outputs=x, name=name)
     return _model
 
 
-def discriminator(noise_shape,
-                  ori_shape,
+def discriminator(ori_shape,
                   name='Discriminator',
                   final_activation='sigmoid'):
-    in_1 = Input(shape=noise_shape)
-    in_2 = Input(shape=ori_shape)
-    x1 = Conv2D(32, 3, padding='same')(in_1)
-    x1 = Activation('elu')(x1)
-    x1 = BatchNormalization()(x1)
-    x1 = Conv2D(32, 3, strides=2, padding='same')(x1)
-    x1 = Activation('elu')(x1)
-    x1 = BatchNormalization()(x1)
-
-    x2 = Conv2D(32, 3, padding='same')(in_2)
-    x2 = Activation('elu')(x2)
-    x2 = BatchNormalization()(x2)
-    x2 = Conv2D(32, 3, strides=2, padding='same')(x2)
-    x2 = Activation('elu')(x2)
-    x2 = BatchNormalization()(x2)
-
-    x = Concatenate()([x1, x2])
-    x = Conv2D(128, 5, padding='same')(x)
+    in_x = Input(shape=ori_shape)
+    x = Conv2D(32, 3, padding='same')(in_x)
     x = Activation('elu')(x)
-    x = Conv2D(256, 5, strides=(2, 2), padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Conv2D(32, 3, strides=2, padding='same')(x)
+    x = Activation('elu')(x)
+    x = BatchNormalization()(x)
+
+    x = inception_block(x, mix=1)
+
+    x = Conv2D(64, 3, strides=2, padding='same')(x)
+    x = Activation('elu')(x)
+    x = BatchNormalization()(x)
+
+    x = inception_block(x, mix=3)
+
+    x = Conv2D(128, 3, strides=2, padding='same')(x)
     x = Activation('elu')(x)
     x = BatchNormalization()(x)
 
     x = Conv2D(256, 5, padding='same')(x)
     x = Activation('elu')(x)
-    x = Conv2D(256, 5, strides=(2, 2), padding='same')(x)
-    x = Activation('elu')(x)
     x = BatchNormalization()(x)
 
-    x = Conv2D(1, 1, activation=final_activation, padding='same')(x)
-    _model = Model(inputs=[in_1, in_2], outputs=x, name=name)
+    x = Conv2D(1, 1, activation=final_activation, padding='same', dtype=tf.float64)(x)
+    _model = Model(inputs=in_x, outputs=x, name=name)
     return _model
 
 
 class DEGAN(Model):
-    def __init__(self, _generator, _discriminator):
+    def __init__(self, _generator, _discriminator, gp_weight=10.0):
         super(DEGAN, self).__init__()
         assert isinstance(_generator, Model)
         assert isinstance(_discriminator, Model)
         self._generator = _generator
         self._discriminator = _discriminator
+        self.gp_weight = gp_weight
 
 
     def compile(self, gen_optimizer, disc_optimizer, gen_loss_fn, disc_loss_fn):
@@ -183,17 +241,44 @@ class DEGAN(Model):
         print_fn('==================================================================================================')
         self._discriminator.summary(line_length=line_length, positions=positions, print_fn=print_fn)
 
+    def gradient_penalty(self, real_images, fake_images):
+        """ Calculates the gradient penalty.
+
+        This loss is calculated on an interpolated image
+        and added to the discriminator loss.
+        """
+        # Get the interpolated image
+        batch_size = tf.shape(real_images)[0]
+        alpha = tf.random.normal([batch_size, 1, 1, 1], 0.0, 1.0, dtype=tf.float64)
+        diff = fake_images - real_images
+        interpolated = real_images + alpha * diff
+
+        with tf.GradientTape() as gp_tape:
+            gp_tape.watch(interpolated)
+            # 1. Get the discriminator output for this interpolated image.
+            pred = self._discriminator(interpolated, training=True)
+
+        # 2. Calculate the gradients w.r.t to this interpolated image.
+        grads = gp_tape.gradient(pred, [interpolated])[0]
+        # 3. Calculate the norm of the gradients.
+        norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=[1, 2, 3]))
+        gp = tf.reduce_mean((norm - 1.0) ** 2)
+        return gp
+
     @ tf.function
     def train_step(self, data):
         data = data_adapter.expand_1d(data)
         noise_img, clean_img, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
             generated_img = self._generator(noise_img, training=True)
-            real_output = self._discriminator([noise_img, clean_img], training=True)
-            gen_output = self._discriminator([noise_img, generated_img], training=True)
+            real_output = self._discriminator(clean_img, training=True)
+            gen_output = self._discriminator(generated_img, training=True)
 
             disc_loss = self.disc_loss_fn(real_output, gen_output)
             gen_loss = self.gen_loss_fn(generated_img, clean_img, gen_output)
+
+            gp_penalty = self.gradient_penalty(clean_img, generated_img)
+            disc_loss += gp_penalty * self.gp_weight
 
         gen_gradient = gen_tape.gradient(gen_loss, self._generator.trainable_variables)
         disc_gradient = disc_tape.gradient(disc_loss, self._discriminator.trainable_variables)
@@ -215,6 +300,7 @@ class DEGAN(Model):
 
         disc_loss = self.disc_loss_fn(real_output, gen_output)
         gen_loss = self.gen_loss_fn(clean_img, generated_img, gen_output)
+        disc_loss += self.gradient_penalty(clean_img, generated_img) * self.gp_weight
         mae = tf.reduce_mean(tf.keras.metrics.mean_absolute_error(clean_img, generated_img))
 
         return {'generator_loss': gen_loss,
@@ -240,7 +326,7 @@ if __name__ == '__main__':
 
     m1 = cnn(input_dim, out)
     m2 = unet(input_dim, out)
-    d = discriminator(input_dim, input_dim)
+    d = discriminator((*input_dim[:2], 1))
 
     # m1.summary()
     # print('===============================')
